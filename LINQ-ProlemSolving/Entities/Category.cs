c63140e312d55468cb207cbd69be8380f31e5c9a
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-18 11:54:30 +0200

    Add project files.

---
+++ LINQ-ProlemSolving/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace LINQ_ProlemSolving.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"CategoryID: {CategoryID}, CategoryName: {CategoryName}, Description: {Description}";
+        }
+    }
+}
---
+++ LINQ-ProlemSolving/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace LINQ_ProlemSolving.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int ShipperID { get; set; }
+        public decimal Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+
+        public override string ToString()
+        {
+            return $"OrderID: {OrderID}, CustomerID: {CustomerID}, EmployeeID: {EmployeeID}, OrderDate: {OrderDate}, RequiredDate: {RequiredDate}, ShippedDate: {ShippedDate}, ShipVia: {ShipperID}, Freight: {Freight}, ShipName: {ShipName}, ShipAddress: {ShipAddress}, ShipCity: {ShipCity}, ShipRegion: {ShipRegion}, ShipPostalCode: {ShipPostalCode}, ShipCountry: {ShipCountry}";
+        }
+    }
+}
